fddc4b8d317dd2a00e23696802c2fda1ea937408
Benoît <bntnowak@outlook.com>
2024-04-10 17:26:15 +0200

    Exercice01-Controllers

--- Exercice01/routes/web.php
+++ Exercice01/routes/web.php
@@ -1,7 +1,15 @@
 <?php
+
+use App\Http\Controllers\ContactController;
 use http\Client\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','welcome',['navbar'=>['/'=>'Acceuil','/apropos'=>'A propos','/contact'=>'Contact']]);
 Route::view('/apropos','apropos',['navbar'=>['/'=>'Acceuil','/apropos'=>'A propos','/contact'=>'Contact']]);
 Route::view('/contact','contact',['navbar'=>['/'=>'Acceuil','/apropos'=>'A propos','/contact'=>'Contact']]);
+
+
+Route::get('/contact',[ContactController::class, 'create']);
+Route::post('/contact',[ContactController::class, 'store']);
+Route::get('/contact/thanks',[ContactController::class, 'thanks']);
+
